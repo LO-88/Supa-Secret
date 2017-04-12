956a5407817ae2ccd9da94dad32357a28e7e6de1
Logan O'Brien <Logan O'Brien>
2017-04-11 21:53:22 -0400

    Created Wire and Event Header Files

--- Wire.h
+++ Wire.h
@@ -1,1 +1,39 @@
-#pragma once
+#ifndef WIRE_H
+#define WIRE_H
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Gate;
+
+class Wire {
+private:
+	string name;
+	short state;
+	vector <Gate*> drives;
+	vector <char> history;
+public:
+	//Define a constant for the undefined value for wire objects
+	const static int UNDEF;
+	
+	//Constructors:
+	Wire(string nameVal) : name(nameVal), state(UNDEF) {}
+
+	//Setter functions:
+	void addGate(Gate*);
+	void setName(string nameVal);
+	void setState(short stateVal);
+
+	//Getter functions:
+	string getName() const;
+	short getState() const;
+	//getters and setters for the vectors?
+
+	//Destructor??
+};
+
+
+
+
+#endif //WIRE_H
