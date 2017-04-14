62444c1f1ae6b4beeb4e082aac5b34b1a8fb3373
Logan O'Brien <Logan O'Brien>
2017-04-14 16:14:39 -0400

    Implemented the member functions of the Wire and Event classes.

---
+++ Event.cpp
@@ -0,0 +1,53 @@
+#include "Event.h"
+
+bool operator<(const Event & firstEvent, const Event & secondEvent)
+{
+	//Check to see if the primary sort is ineffective. If
+	//it is, use the event number as the secondary sort.
+	if (firstEvent.time == secondEvent.time) {
+		return firstEvent.eventNum < secondEvent.eventNum;
+	}
+	else return firstEvent.time < secondEvent.time;
+}
+
+int Event::getEventNum() const
+{
+	return eventNum;
+}
+
+int Event::getEventValue() const
+{
+	return eventValue;
+}
+
+int Event::getTime() const
+{
+	return time;
+}
+
+int Event::getWireNum() const
+{
+	return wireNum;
+}
+
+void Event::setEventNum(int eventNumVal)
+{
+	eventNum = eventNumVal;
+}
+
+void Event::setEventValue(int eventVal)
+{
+	eventValue = eventVal;
+}
+
+void Event::setTime(int timeVal)
+{
+	time = timeVal;
+}
+
+void Event::setWireNum(int wireNumVal)
+{
+	wireNum = wireNumVal;
+}
+
+
--- Wire.cpp
+++ Wire.cpp
@@ -2,3 +2,28 @@
 //Initialize the static variable so that it
 //can be used as a sentinal value.
 const int Wire::UNDEF = -1;
+
+void Wire::addGate(Gate * gate)
+{
+	drives.push_back(gate);
+}
+
+void Wire::setName(string nameVal)
+{
+	name = nameVal;
+}
+
+void Wire::setState(short stateVal)
+{
+	state = stateVal;
+}
+
+string Wire::getName() const
+{
+	return name;
+}
+
+short Wire::getState() const
+{
+	return state;
+}
